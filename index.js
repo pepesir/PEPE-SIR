8f2b2e2686038f5d999e30bf0a55706528f6402c
Xeon <88829846+DGXeon@users.noreply.github.com>
2021-10-31 11:21:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -373,7 +373,7 @@ const checkRegisteredUser = (sender) => {
     const totalhit = JSON.parse(fs.readFileSync("./lib/totalcmd.json"))[0]
       .totalcmd;
 const daftar1 = `Hi ${pushname} ${ucapanWaktu} \n\nBefore Using the Bot Verify First `
-       const daftar2 = '```Click the button below to verify bro or type 8473\n𝐃𝐨𝐠𝐞 𝐁𝐨𝐭```'
+       const daftar2 = '```Click the button below to verify bro or type #verify\n𝐃𝐨𝐠𝐞 𝐁𝐨𝐭```'
        const daftar3 = [
           {
             buttonId: `verify`,
