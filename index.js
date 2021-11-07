52a97df1c2aab440968df418a146213a8c60f8e5
pepesir <90826638+pepesir@users.noreply.github.com>
2021-11-07 06:59:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Script by Pepe
 // 𝙿𝙴𝙿𝙴 𝚂𝙸𝚁 | 𝙳𝙾𝙽'𝚃 𝙲𝙷𝙰𝙽𝙶𝙴 𝙲𝚁𝙴𝙳𝙸𝚃𝚂🙂
-// ©Pepe Bot
+// ©Pepe Bot Incorporated
 const {
   WAConnection: _WAConnection,
   MessageType,
@@ -5945,7 +5945,7 @@ Source : ${anu.result.source}
                     thumb,
                     `*Data Successfully Obtained!*\n\n*Title* : ${title}\n*Ext* : MP3\n*Filesize* : ${filesizeF}\n*Link* : ${a.data}\n\nFor the duration of more than the limit is presented in the link`
                   );
-                const captions = `╭──────────────────╮\n*      ʏᴛᴍᴘ3*\n╭──────────────────╯\n│ *ᴛɪᴛʟᴇ* : ${title}\n│ *ᴇxᴛ* : MP3\n╰──────────────────╮\n *sɪᴢᴇ* : ${filesizeF}\n──────────────────╯\n❑ Please wait for the media file to be sent it may take a few minutes`;
+                const captions = `╭──────────────────╮\n         *ʏᴛᴍᴘ3*\n╭──────────────────╯\n│ *ᴛɪᴛʟᴇ* : ${title}\n│ *ᴇxᴛ* : MP3\n╰──────────────────────────╮\n *sɪᴢᴇ* : ${filesizeF}\n ──────────────────────────╯\n❑ Please wait for the media file to be sent it may take a few minutes\n ©ᴘᴇᴘᴇ`;
                 sendMediaURL(from, thumb, captions);
                 sendMediaURL(from, dl_link).catch(() => reply(mess.error.api));
               });
